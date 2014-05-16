ad63975da644d81323e942e15a45654445610a23
dkaplan <dkaplan@enova.com>
2014-05-15 23:23:04 -0500

    better regex

--- phrase.rb
+++ phrase.rb
@@ -6,8 +6,7 @@ class Phrase
   def word_count
     phrase_hsh = {}
     phrase_arr = @phrase.downcase
-                        .split(/[^a-zA-Z0-9']+/)
-                        .delete_if { |w| w.empty? }
+                        .split(/[^a-zA-Z0-9(')('')]+/)
 
     phrase_arr.uniq.each do |word|
       phrase_hsh.merge!({ word => phrase_arr.count(word)})
